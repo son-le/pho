936daf8f2ed2346009bd0690ea64274818374459
Son Le <sillyson@gmail.com>
2013-03-01 03:27:18 -0500

    Expand tabs to spaces.

--- test/api.js
+++ test/api.js
@@ -6,39 +6,39 @@ var expect = require( "expect.js" )
 
 describe( "API", function() {
 
-	it( "defines mixin( _ ) function", function() {
+    it( "defines mixin( _ ) function", function() {
 
-		expect( _.isFunction( pho.mixin ) ).to.be.ok();
-		expect( pho.mixin.length ).to.be( 1 );
-	});
+        expect( _.isFunction( pho.mixin ) ).to.be.ok();
+        expect( pho.mixin.length ).to.be( 1 );
+    });
 
 
 
-	it( "defines onErr( err, callback ) function", function() {
+    it( "defines onErr( err, callback ) function", function() {
 
-		expect( _.isFunction( pho.onErr ) ).to.be.ok();
-		expect( pho.onErr.length ).to.be( 2 );
-	});
+        expect( _.isFunction( pho.onErr ) ).to.be.ok();
+        expect( pho.onErr.length ).to.be( 2 );
+    });
 });
 
 
 
 describe( "Underscore mixin API", function() {
 
-	var pho2 = pho.mixin( _ );
+    var pho2 = pho.mixin( _ );
 
 
 
-	it ( 'mixin( _ ) function returns same object as require( "pho" )', function() {
+    it ( 'mixin( _ ) function returns same object as require( "pho" )', function() {
 
-		expect( pho ).to.be( pho2 );
-	});
+        expect( pho ).to.be( pho2 );
+    });
 
 
 
-	it( "defines onErr( err, callback ) function on underscore", function() {
+    it( "defines onErr( err, callback ) function on underscore", function() {
 
-		expect( _.isFunction( _.onErr ) ).to.be.ok();
-		expect( _.onErr.length ).to.be( 2 );
-	});
+        expect( _.isFunction( _.onErr ) ).to.be.ok();
+        expect( _.onErr.length ).to.be( 2 );
+    });
 });
